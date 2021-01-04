7c4ce6b2785bca6b1e5d0343c871b83a610deada
Kim-fx <jkingzk.j@gmail.com>
2021-01-04 20:48:44 +0100

    update

--- src/pages/review.js
+++ src/pages/review.js
@@ -1,15 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import HelloWorld from "./components/HelloWorld";
 
-function Review() {
-  return (
-    <div className="container">
-      <HelloWorld />
-    </div>
-  );
-}
- export default Review;
 
